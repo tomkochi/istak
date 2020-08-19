b597ad8241b2a74f6c50d46faf17d0947e9e8dcd
Tinjo Thomas <tinjothomasc@gmail.com>
2020-08-19 15:03:08 +0530

    anim fixed

--- pages/thjonusta/index.js
+++ pages/thjonusta/index.js
@@ -1,31 +1,32 @@
-import Hero from '../../components/services/hero'
-import Layout from '../../components/Layout'
-import CardImageRight from '../../components/services/service-card-image-at-right'
-import CardImageLeft from '../../components/services/service-card-image-at-left'
+import Hero from "../../components/services/hero";
+import Layout from "../../components/Layout";
+import CardImageRight from "../../components/services/service-card-image-at-right";
+import CardImageLeft from "../../components/services/service-card-image-at-left";
 
 const ServicesIndex = (props) => {
+  const title = { title: "Þjónstudeildir" };
   return (
     <>
       <Layout>
-        <Hero />
+        <Hero data={title} />
         {props.data.map((s, i) => {
           return i % 2 ? (
             <CardImageRight key={s.id} data={s} />
           ) : (
             <CardImageLeft key={s.id} data={s} />
-          )
+          );
         })}
       </Layout>
     </>
-  )
-}
+  );
+};
 
 export async function getServerSideProps() {
   // Fetch data from external API
-  const res = await fetch(`${process.env.HOST}/services`)
-  const data = await res.json()
+  const res = await fetch(`${process.env.HOST}/services`);
+  const data = await res.json();
   // Pass data to the page via props
-  return { props: { data } }
+  return { props: { data } };
 }
 
-export default ServicesIndex
+export default ServicesIndex;
