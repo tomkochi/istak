2eb490d342ecc989e7d04be1138de912f7f23797
tomkochi <tomatkochi@gmail.com>
2020-10-26 16:30:49 +0530

    search added to articles page too

--- components/news/hero.js
+++ components/news/hero.js
@@ -5,27 +5,6 @@ const Hero = (props) => {
         <h1 className="f-gtam-bold wow fadeIn">
           Lestu nýjustu greinar og uppfærslur
         </h1>
-        <div className="search d-flex wow fadeIn" data-wow-delay="500ms">
-          <input
-            type="text"
-            className="f-gtam-medium"
-            placeholder="Leita að grein"
-          />
-          <div className="icon d-flex align-items-center justify-content-center">
-            <svg
-              width="15"
-              height="15"
-              viewBox="0 0 15 15"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <circle cx="6" cy="6" r="5" stroke="#204F9C" strokeWidth="2" />
-              <path d="M10 10L13.5 13.5" stroke="#204F9C" strokeWidth="2" />
-            </svg>
-          </div>
-          {/* .icon */}
-        </div>
-        {/* .search */}
       </div>
       {/* .hero */}
 
@@ -60,59 +39,10 @@ const Hero = (props) => {
               font-size: 36px;
             }
           }
-          .search {
-            margin-bottom: 117px;
-            width: 100%;
-            max-width: 400px;
-            position: relative;
-            @media (max-width: 767px) {
-              margin-bottom: 80px;
-            }
-            input {
-              border: none;
-              font-size: 18px;
-              line-height: 120%;
-              padding: 0;
-              margin-left: 57px;
-              outline: none;
-              box-shadow: none !important;
-              &::placeholder {
-                /* Chrome, Firefox, Opera, Safari 10.1+ */
-                color: #5d749a;
-              }
-              &:-ms-input-placeholder {
-                /* Internet Explorer 10-11 */
-                color: #5d749a;
-              }
-              &::-ms-input-placeholder {
-                /* Microsoft Edge */
-                color: #5d749a;
-              }
-              &:focus + .icon {
-                background: $brand;
-                svg * {
-                  stroke: #ffffff;
-                }
-              }
-            }
-            .icon {
-              position: absolute;
-              top: -8px;
-              width: 42px;
-              height: 42px;
-              border: 1px solid rgba(32, 79, 156, 0.2);
-              border-radius: 50%;
-              transition: 0.3s;
-              svg path,
-              svg circle {
-                transition: 0.3s;
-              }
-            }
-          }
         }
       `}</style>
     </>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
--- pages/frettir/index.js
+++ pages/frettir/index.js
@@ -1,36 +1,65 @@
-import Layout from '../../components/Layout'
-import Hero from '../../components/news/hero'
-import NewsCard from '../../components/news/news-card'
+import { useEffect, useState } from "react";
+import Layout from "../../components/Layout";
+import Filter from "../../components/news/filter";
+import Hero from "../../components/news/hero";
+import NewsCard from "../../components/news/news-card";
 
 const Index = (props) => {
+  const [articleList, setArticleList] = useState([]);
+
+  const search = (text) => {
+    let newList = props.data;
+    if (text) {
+      newList = props.data.filter((d) => {
+        return d.title.toLowerCase().search(text.toLowerCase()) !== -1;
+      });
+    }
+    setArticleList(newList);
+  };
+
+  useEffect(() => {
+    search();
+  }, []);
+
   return (
     <Layout>
       <div className="articles">
         <Hero />
-        {props.data.map((e) => {
-          return (
-            <NewsCard
-              key={e._id}
-              image={`${process.env.NEXT_PUBLIC_HOST}${e.image.formats.medium.url}`}
-              title={e.title}
-              link={`/frettir/${e.slug}`}
-            />
-          )
-        })}
+        <Filter onSearchChange={search} />
+        {articleList.length ? (
+          articleList.map((e) => {
+            return (
+              <NewsCard
+                key={e._id}
+                image={`${process.env.NEXT_PUBLIC_HOST}${e.image.formats.medium.url}`}
+                title={e.title}
+                link={`/frettir/${e.slug}`}
+              />
+            );
+          })
+        ) : (
+          <div className="not-found">No records found</div>
+        )}
       </div>
       {/* .articles */}
+      <style jsx>{`
+        .not-found {
+          text-align: center;
+          margin-bottom: 90px;
+        }
+      `}</style>
     </Layout>
-  )
-}
+  );
+};
 
 //data fetching
 export async function getServerSideProps(context) {
   // Fetch data from external API
-  const res = await fetch(`${process.env.HOST}/articles`)
-  const data = await res.json()
+  const res = await fetch(`${process.env.HOST}/articles`);
+  const data = await res.json();
 
   // Pass data to the page via props
-  return { props: { data } }
+  return { props: { data } };
 }
 
-export default Index
+export default Index;
